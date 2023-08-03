5672fb8ee109bda06986028519f7d1013d733bf1
sammyALX <samuelhailu49@gmail.com>
2023-08-03 17:04:07 +0300

    annotate RedactingFormatter constructor params

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,27 +7,6 @@ import re
 from typing import List
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """custom formatting logRecord
-        """
-        return filter_datum(self.fields,
-                            self.REDACTION,
-                            super(RedactingFormatter, self).format(record),
-                            self.SEPARATOR)
-
-
 def filter_datum(fields: List[str],
                  redaction: str,
                  message: str,
@@ -39,3 +18,24 @@ def filter_datum(fields: List[str],
                          '{}={}{}'.format(field, redaction, separator),
                          message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """custom formatting logRecord
+        """
+        return filter_datum(self.fields,
+                            self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
