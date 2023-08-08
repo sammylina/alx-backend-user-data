3c36f05eb2890784ca08d990a25e0e815899a9ab
sammyALX <samuelhailu49@gmail.com>
2023-08-08 12:49:22 +0300

    add base authentication class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Auth module
+"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Authentication class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Decide if authentication is required or not
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Parse values from authorization header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Provide current user, if any
+        """
+        return None
