1bb523db870b99cfd44239478c1bb6d0385ad353
sammyALX <samuelhailu49@gmail.com>
2023-08-08 12:20:22 +0300

    handle unauthorized error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> str:
+    """ Handle unauthorized request
+    """
+    abort(401)
